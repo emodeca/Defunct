96ac6b682d9ceb0b24cda6333b84d592551f047b
emodeca <mike.s.c@gmail.com>
2015-03-24 20:53:12 -0400

    Fixed enabled text boxes in ticketer

--- js/ticketer.js
+++ js/ticketer.js
@@ -2,14 +2,19 @@ $(function(){
 	$(":checkbox").click(function(){
 		if ($(this).prop("checked")) {
 
+			
 			$(this).parent().children(":text").fadeTo("slow" , 0, function() {
+				$(this).prop("disabled", true)
     			// Animation complete.);
-			});
-
-	} else {
-		$(this).parent().children(":text").fadeTo("slow" , 1, function() {
-    			// Animation complete.);
-			});
-	}
-});
 		});
+
+		} else {
+			
+			$(this).parent().children(":text").fadeTo("slow" , 1, function() {
+				$(this).prop("disabled", false)
+    			// Animation complete.);
+
+		});
+		}
+	});
+});
