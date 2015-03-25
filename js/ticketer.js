8cb41d28b869d30bc35acbecc7244f0b7a04c0c4
emodeca <mike.s.c@gmail.com>
2015-03-24 21:51:01 -0400

    Beginning String Building

--- js/ticketer.js
+++ js/ticketer.js
@@ -1,19 +1,23 @@
+var allInputs;
+var toAppend = ["Store #:", "Caller's Name:", "iPad Identifier:", "Verifone Sled Serial #:"];
+
 $(function(){
+
 	$(":checkbox").click(function(){
 		if ($(this).prop("checked")) {
-
-			
-			$(this).parent().children(":text").fadeTo("slow" , 0, function() {
-				$(this).prop("disabled", true);
-    			// Animation complete.);
-		});
-
+			$(this).parent().children(":text").fadeTo("slow", 0, function(){$(this).prop("disabled", true);});
 		} else {
 			$(this).parent().children(":text").prop("disabled", false);
-			$(this).parent().children(":text").fadeTo("slow" , 1, function() {
-    			// Animation complete.);
-
-		});
+			$(this).parent().children(":text").fadeTo("slow", 1, function(){});
 		}
+	});
+	$("#submit").click(function(){
+		var lawg = '';
+		allInputs = $( ":input" );
+		for (i = 0; i < 4; i++){
+			lawg += toAppend[i] + " " +allInputs[i].value + "\n";
+		}
+		console.log(lawg);
+
 	});
 });
